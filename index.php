09ed82ac4a8328dc3f1a232b2caa142824a4e863
gdelnegro <gdelnegro@ubuntu>
2014-01-03 18:34:56 -0200

    Criado model de Grupo de usuário; Criado form de Grupo de usuário; Criadas views referentes ao Grupo de usuário; Editado Controller de Grupo

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        
+	$rss = new DOMDocument();
+	$rss->load('http://agilmotoboy.com.br/blog/feed/');
+	$feed = array();
+	foreach ($rss->getElementsByTagName('item') as $node) {
+		$item = array ( 
+			'title' => $node->getElementsByTagName('title')->item(0)->nodeValue,
+			'desc' => $node->getElementsByTagName('description')->item(0)->nodeValue,
+			'link' => $node->getElementsByTagName('link')->item(0)->nodeValue,
+			'date' => $node->getElementsByTagName('pubDate')->item(0)->nodeValue,
+			);
+		array_push($feed, $item);
+	}
+	$limit = 3;
+	for($x=0;$x<$limit;$x++) {
+		$title = str_replace(' & ', ' &amp; ', $feed[$x]['title']);
+		$link = $feed[$x]['link'];
+		$description = $feed[$x]['desc'];
+		$date = date('l F d, Y', strtotime($feed[$x]['date']));
+		echo '<p><strong><a href="'.$link.'" target="_blank" title="'.$title.'">'.$title.'</a></strong><br />';
+		echo '<small><em>Postado em '.$date.'</em></small></p>';
+		echo '<p>'.$description.'</p>';
+	}
+        ?>
+    </body>
+</html>
